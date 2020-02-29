4810aab0c62bf5a654ec24a7fb256dfbec790f46
Erzhena <erzhena.tsyrendylykova@gmail.com>
2020-02-29 20:24:11 +0300

    [add] pre-final version

--- holograms/urls.py
+++ holograms/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.holograms, name='holograms'),
+    path('fa30n', views.fa30n, name="fa30n"),
+    path('fa42hd', views.fa42hd, name="fa42hd"),
+    path('fa65n', views.fa65n, name="fa65n"),
+    path('fa70n', views.fa70n, name="fa70n"),
+    path('fa85n', views.fa85n, name="fa85n"),
+    path('fa100n', views.fa100n, name="fa100n"),
 ]
--- holograms/views.py
+++ holograms/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def holograms(request):
     return render(request, 'holograms/index.html', {})
+
+def fa30n(request):
+    return render(request, 'holograms/fa30n.html', {})
+
+def fa42hd(request):
+    return render(request, 'holograms/fa42hd.html', {})
+
+def fa65n(request):
+    return render(request, 'holograms/fa65n.html', {})
+
+def fa70n(request):
+    return render(request, 'holograms/fa70n.html', {})
+
+def fa85n(request):
+    return render(request, 'holograms/fa85n.html', {})
+
+def fa100n(request):
+    return render(request, 'holograms/fa100n.html', {})
