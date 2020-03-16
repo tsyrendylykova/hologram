4f226bea40cdc25cf281d5a6c8b6e6a87cee6630
Erzhena <erzhena.tsyrendylykova@gmail.com>
2020-03-16 22:51:29 +0300

    add allowed hosts

--- holo/settings.py
+++ holo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%53o&b)w%gv6mv7)fbjcpr!mbv17$z%5jzgo(=!=l3+mybldxy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tsyrendylykova.pythonanywhere.com', 'feelFit.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tsyrendylykova.pythonanywhere.com', 'feelFit.pythonanywhere.com', 'www.holoproject.ru', 'holoproject.ru']
 
 
 # Application definition
